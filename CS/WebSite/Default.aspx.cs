638f69ca0670e825cf0f89d0448ce86099bd4bfe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+    }
+    protected void dsProducts_Updating(object sender, SqlDataSourceCommandEventArgs e) {
+        //Remove the next line to enable data updating
+        throw new Exception("Data updating is not allowed in examples.");
+    }
+}
